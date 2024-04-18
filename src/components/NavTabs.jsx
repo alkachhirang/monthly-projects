6c3393c1215df2730dd8b4f06fc7dd3beb9ee610
alkachhirang <alkachhirang3@gmail.com>
2024-04-18 12:31:30 +0530

    add project

--- src/components/NavTabs.jsx
+++ src/components/NavTabs.jsx
@@ -20,6 +20,7 @@ import projectimg from '../assets/images/png/project-img.png';
 import todoimg from '../assets/images/png/todoimg.png';
 import nobelmind from '../assets/images/png/nobel-mind.png';
 import miniminears from '../assets/images/png/mini-miners.png';
+import fig from '../assets/images/png/8fig.png';
 
 
 
@@ -348,7 +349,7 @@ const NavTabs = () => {
                         <div className='flex-row flex flex-wrap items-center justify-between mb-[40px]'>
                             <div className='w-[35%] flex gap-[16px]'>
                                 <p className='text-black font-semibold text-[30px]'>10.</p>
-                                <img src={todoimg} alt="todoimg" className='w-full rounded-[20px]'/>
+                                <img src={todoimg} alt="todoimg" className='w-full rounded-[20px]' />
                             </div>
                             <div className='w-[55%]'>
                                 <div className='mb-[18px]'>
@@ -384,7 +385,7 @@ const NavTabs = () => {
                         <div className='flex-row flex flex-wrap items-center justify-between mb-[40px]'>
                             <div className='w-[35%] flex gap-[16px]'>
                                 <p className='text-black font-semibold text-[30px]'>2.</p>
-                            <img src={miniminears} alt="miniminears" className='w-full rounded-[20px]' />
+                                <img src={miniminears} alt="miniminears" className='w-full rounded-[20px]' />
                             </div>
                             <div className='w-[55%]'>
                                 <div className='mb-[18px]'>
@@ -411,6 +412,23 @@ const NavTabs = () => {
                                 <div>
                                     <p className='text-black font-semibold text-[24px] mb-[10px]'>Live-link :</p>
                                     <a href='https://somereact-topics.vercel.app/' target='blank' className='text-black font-medium text-[18px] hover:text-sky-500 transition-all ease-linear duration-300'>https://somereact-topics.vercel.app/</a>
+                                </div>
+                            </div>
+
+                        </div>
+                        <div className='flex-row flex flex-wrap items-center justify-between mb-[40px]'>
+                            <div className='w-[35%] flex gap-[16px]'>
+                                <p className='text-black font-semibold text-[30px]'>4.</p>
+                                <img src={fig} alt="reactimg" className='w-full rounded-[20px]' />
+                            </div>
+                            <div className='w-[55%]'>
+                                <div className='mb-[18px]'>
+                                    <p className='text-black font-semibold text-[24px] mb-[10px]'>Github-link :</p>
+                                    <a href='https://github.com/alkachhirang/8fig' target='blank' className='text-black font-medium text-[18px] hover:text-sky-500 transition-all ease-linear duration-300'>https://github.com/alkachhirang/8fig</a>
+                                </div>
+                                <div>
+                                    <p className='text-black font-semibold text-[24px] mb-[10px]'>Live-link :</p>
+                                    <a href='https://8fig-eosin.vercel.app/' target='blank' className='text-black font-medium text-[18px] hover:text-sky-500 transition-all ease-linear duration-300'>https://8fig-eosin.vercel.app/</a>
                                 </div>
                             </div>
 
